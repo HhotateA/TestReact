40cce8aa9dc44e9c48db6860c062a30abcd14cc5
HhotateA <HhotateA@gmail.com>
2020-09-10 13:36:08 +0900

    format

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button';
 import './App.css';
 import './index.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      todos: [ ],
+      todos: [],
       newId: 0,
       newTitle: "New Task",
     }
@@ -21,13 +21,13 @@ class App extends Component {
     todos[clickTodo.id] = todo;
     console.log(todo.title + " : " + todo.done);
     console.log(this.state.todos);
-    this.setState({todos});
+    this.setState({ todos });
   }
-  
+
   // TextFieldの入力
   handleChange(e) {
     console.log(e.target.value);
-    this.setState({newTitle: e.target.value});
+    this.setState({ newTitle: e.target.value });
   }
 
   // 新たなタスクの追加
@@ -36,7 +36,7 @@ class App extends Component {
     let newId = this.state.newId;
     let newTitle = this.state.newTitle;
     var DD = new Date();
-    var dateTime = DD.getHours()+"時 "+DD.getMinutes()+"分 "+DD.getSeconds()+"秒";
+    var dateTime = DD.getHours() + "時 " + DD.getMinutes() + "分 " + DD.getSeconds() + "秒";
     const newTask = {
       id: newId,
       timeStamp: dateTime,
@@ -45,27 +45,27 @@ class App extends Component {
     }
     newId += 1;
     todos.push(newTask);
-    this.setState({todos: todos});
-    this.setState({newId: newId});
-    this.setState({newTitle: newTitle});
+    this.setState({ todos: todos });
+    this.setState({ newId: newId });
+    this.setState({ newTitle: newTitle });
   }
 
-  render(){
+  render() {
     console.log("reload");
     return (
-      <div className = "App">
+      <div className="App">
         <h2> TodoList </h2>
         <TodoList
-          todos = {this.state.todos}
-          changeState = {this.changeState.bind(this)}
+          todos={this.state.todos}
+          changeState={this.changeState.bind(this)}
         />
         <div>
           <form>
             <TextField
-              name = "aa"
-              id = "NewTaskField"
-              label = {this.state.newTitle}
-              onChange = {this.handleChange.bind(this)}
+              name="aa"
+              id="NewTaskField"
+              label={this.state.newTitle}
+              onChange={this.handleChange.bind(this)}
             />
             <Button onClick={this.addNewTask.bind(this)}> AddTask </Button>
           </form>
@@ -77,17 +77,17 @@ class App extends Component {
 
 class TodoList extends Component {
   render() {
-    const todos = this.props.todos.map( todo =>
+    const todos = this.props.todos.map(todo =>
       <Todo
-        key = {todo.id}
-        id = {todo.id}
-        timeStamp = {todo.timeStamp}
-        title = {todo.title}
-        done = {todo.done}
-        changeState = {this.props.changeState}
+        key={todo.id}
+        id={todo.id}
+        timeStamp={todo.timeStamp}
+        title={todo.title}
+        done={todo.done}
+        changeState={this.props.changeState}
       />
     );
-    return(
+    return (
       <ul>
         {todos}
       </ul>
@@ -97,18 +97,26 @@ class TodoList extends Component {
 
 class Todo extends Component {
   render() {
-    if(this.props.done){
+    if (this.props.done) {
       return null;
     } else {
-      return(
+      return (
         <li className="Cell">
-          <span style={{float:"left"}}> {this.props.timeStamp} </span>
+          <span style={{ float: "left" }}> {this.props.timeStamp} </span>
           <span> {this.props.title} </span>
-          <Button style={{float:"right"}} onClick={(e)=>{ e.preventDefault(); this.props.changeState(this.props)}}> 完了 </Button>
-        </li>
+          <Button
+            style={{ float: "right" }}
+            onClick={(e) => {
+              e.preventDefault();
+              this.props.changeState(this.props)
+            }}
+          />
+            完了
+          </Button>
+        </li >
       );
     }
-  } 
+  }
 }
 
 export default App;
